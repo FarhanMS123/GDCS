d62ec3dcf83f07544b59b2e55498962e0ce3c9e4
Farhan <animationfar@gmail.com>
2023-03-02 23:13:44 +0700

    feat: Connector

---
+++ src/components/Main/Connector.tsx
@@ -0,0 +1,58 @@
+import { useCardStyles } from "@/styles/card.styles";
+import { Button, Input, Label, makeStyles, mergeClasses, shorthands, Text, tokens, Toolbar, ToolbarButton, useId } from "@fluentui/react-components";
+import { Wrench16Filled, Wrench20Filled, WrenchFilled } from "@fluentui/react-icons";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+
+const useConnectorStyles = makeStyles({
+  root: {
+    width: '16rem',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  textContentBefore: {
+    ...shorthands.borderRight(tokens.strokeWidthThin, 'solid', tokens.colorNeutralStroke1),
+    paddingRight: tokens.spacingHorizontalS,
+    marginRight: tokens.spacingHorizontalXS
+  },
+  buttonContainer: {
+    marginTop: tokens.spacingVerticalS,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  toolbar: {
+    justifyContent: 'flex-end',
+    marginTop: '0px',
+  },
+})
+
+export type Status = 'closed' | 'established' | 'wait';
+export type StateStatus = [Status, Dispatch<SetStateAction<Status>>];
+
+/**
+ * Create Room, Connect, Disconnect, Reconnect, Generate
+ * @returns 
+ */
+
+export default function Connector(){
+  const [status, setStatus]: StateStatus = useState('closed' as Status);
+  const classes = useConnectorStyles();
+  const c_card = useCardStyles();
+
+  if (status == 'wait') setTimeout(() => { setStatus('established') }, 3000);
+
+  return (
+    <div className={mergeClasses(c_card.root, classes.root)}>
+      <Input appearance="filled-lighter-shadow" contentBefore={<Text className={classes.textContentBefore} weight="medium">Room</Text>} />
+      <div className={classes.buttonContainer}>
+        <Button disabled={ status != 'closed' }>Create Room</Button>
+        <div>
+          { status == 'closed' && <Button appearance="primary" onClick={() => setStatus('wait')}>Connect</Button>}
+          { status == 'wait' && <Button appearance="primary" disabled>Connecting</Button>}
+          { status == 'established' && <Button appearance="primary" onClick={() => setStatus('closed')}>Disconnect</Button>}
+          <Button appearance="subtle" icon={ <WrenchFilled /> } />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,20 @@
-import { FluentProvider, teamsDarkTheme } from '@fluentui/react-components';
+import 'normalize.css';
+
+import { FluentProvider, makeStyles, shorthands, teamsDarkTheme, tokens } from '@fluentui/react-components';
 import type { AppProps } from 'next/app'
 
+const useAppStyles = makeStyles({
+  root: {
+    width: '100vw',
+    height: '100vh',
+    // ...shorthands.overflow('scroll', 'auto'),
+  },
+});
+
 export default function App({ Component, pageProps }: AppProps) {
+  const classes = useAppStyles();
   return (
-    <FluentProvider theme={teamsDarkTheme}>
+    <FluentProvider theme={teamsDarkTheme} className={classes.root}>
       <Component {...pageProps} />
     </FluentProvider>
   );
